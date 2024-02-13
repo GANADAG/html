303f144a5cb88bcd3e17fc851e7b624eff175a19
LEEHYUNJU <dl06797@naver.com>
2024-02-13 14:11:33 +0900

    0213

---
+++ jsquerywork/js/popup.js
@@ -0,0 +1,12 @@
+function pop1()
+{
+    window.open('../image/쇼핑몰사진/1.jpg','event','width=500,height=500');
+}
+
+function pop2()
+{
+    newpopup=window.open('../image/logoImg/box.png','','wigth=40,height=500');
+
+   setTimeout('newpopup.close()',3000);
+//    3초뒤 닫음
+}
